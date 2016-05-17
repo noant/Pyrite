d042f1b48b6c9fd580eb083cd3f12a4d2603eb22
noant <anton.novgorodcev@gmail.com>
2016-05-17 23:30:27 +0300

    ---

--- Pyrite/PyriteStandartActions/DoNotDeleteThisClass.cs
+++
@@ -1,7 +0,0 @@
-﻿
-namespace PyriteStandartActions
-{
-    public class DoNotDeleteThisClass
-    {
-    }
-}
--- PyriteRemoteForAndroid/app/src/main/java/com/example/notebookuser/customremote/ActionPairRunnable.java
+++
@@ -1,8 +0,0 @@
-package com.example.notebookuser.customremote;
-
-/**
- * Created by NotebookUser on 01.02.2016.
- */
-public abstract class ActionPairRunnable {
-    public abstract void run(ActionPair actionPair);
-}
