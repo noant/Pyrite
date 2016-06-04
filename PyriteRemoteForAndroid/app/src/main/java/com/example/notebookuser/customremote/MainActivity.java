440878d0db50e347dcfa4356afdb402cc9420802
anton.novgorodcev <anton.novgorodcev@gmail.com>
2016-06-04 15:01:44 +0300

    PyriteRemote.apk update

--- PyriteRemoteForAndroid/app/src/main/java/com/example/notebookuser/customremote/MainActivity.java
+++ PyriteRemoteForAndroid/app/src/main/java/com/example/notebookuser/customremote/MainActivity.java
@@ -20,6 +20,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
         new Statics().init();
         refresh();
         if (!_timerStarted)
